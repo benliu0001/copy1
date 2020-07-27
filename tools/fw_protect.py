0aadcc91ae50c32f0fd649135c1daaff9a0a83ef
coraskidmore <coraskidmore@gmail.com>
2020-07-27 19:55:19 +0000

    7/27.5

--- tools/fw_protect.py
+++ tools/fw_protect.py
@@ -27,22 +27,11 @@ def protect_firmware(infile, outfile, version, message):
 
         seed = sbo.read(16)
         a = struct.unpack('H', sbo.read(2))[0]
-        print(a)
         b = struct.unpack('H', sbo.read(2))[0]
         c = struct.unpack('H', sbo.read(2))[0]
         d = struct.unpack('H', sbo.read(2))[0]
         e = struct.unpack('H', sbo.read(2))[0]
-#         aeskey = sbo.read(16)    - - this is for no stream cipher
-#         firmkey = sbo.read(16)
-#         metakey = sbo.read(16)
 
-        #if we were to have a seed, would happen here??
-    print(aeskey)
-    print(len(aeskey))
-    print(firmkey)
-    print(len(firmkey))
-    print(metakey)
-    print(len(metakey))
     # Load firmware binary from infile
     with open(infile, 'rb') as fp:
         firmware = fp.read()
@@ -67,10 +56,9 @@ def protect_firmware(infile, outfile, version, message):
 
     with open(outfile, 'wb') as f:
         f.write(metadata)
-        # do we write the HMAC here as well?
         
 
-    # split into 1024 bytes and encrypting it 
+    # split into 1024 bytes and encrypting it with GCM (using AES)
     for i in range(0,len(firmware_and_message),1024):
         #double check the <h1024s??
         whatwewant = firmware_and_message[i:i+1024]
@@ -101,114 +89,3 @@ if __name__ == '__main__':
 
     protect_firmware(infile=args.infile, outfile=args.outfile, version=int(args.version), message=args.message)
 
-
-# '''
-# #stream cipher: KSA() is function for making key - K
-# #               PRGA() is function for making initial vector - S
-
-# MOD = 256
-
-# def KSA(key):
-# '''
-# ''' Key Scheduling Algorithm (from wikipedia):
-#     for i from 0 to 255
-#         S[i] := i
-#     endfor
-#     j := 0
-#     for i from 0 to 255
-#         j := (j + S[i] + key[i mod keylength]) mod 256
-#         swap values of S[i] and S[j]
-#     endfor
-# '''
-# '''
-#   key_length = len(key)
-#   # create the array "S"
-#   S = range(MOD)  # [0,1,2, ... , 255]
-#   j = 0
-#   for i in range(MOD):
-#     j = (j + S[i] + key[i % key_length]) % MOD
-#     S[i], S[j] = S[j], S[i]  # swap values
-
-#   return S
-
-
-# def PRGA(S):
-# '''
-# ''' Psudo Random Generation Algorithm (from wikipedia):
-#     i := 0
-#     j := 0
-#     while GeneratingOutput:
-#         i := (i + 1) mod 256
-#         j := (j + S[i]) mod 256
-#         swap values of S[i] and S[j]
-#         K := S[(S[i] + S[j]) mod 256]
-#         output K
-#     endwhile
-# '''
-# '''
-#   i = 0
-#   j = 0
-#   while True:
-#     i = (i + 1) % MOD
-#     j = (j + S[i]) % MOD
-
-#     S[i], S[j] = S[j], S[i]  # swap values
-#     K = S[(S[i] + S[j]) % MOD]
-#     yield K
-
-
-# def get_keystream(key):
-#     '''
-#     ''' Takes the encryption key to get the keystream using PRGA
-#         return object is a generator
-#     '''
-#     '''
-#   S = KSA(key)
-#   return PRGA(S)
-
-
-# #function to encrypt plaintext (eventually firmware?) with key from secret...txt file: encrypy()
-# #func to decrypt: decryption using key from secret_build_output.txt file and ciphertext:
-
-# def encrypt(keystream, plaintext):    
-#   res = []
-#   for c in plaintext:
-#     val = ("%02X" % (ord(c) ^ next(keystream)))  # XOR and taking hex
-#     res.append(val)
-#   return ''.join(res)
-
-
-# def decrypt(keystream, ciphertext):
-#     #keystream -> encryption key used for encrypting, as hex string
-#      #ciphertext -> hex encoded ciphered text using RC4
-    
-#   ciphertext = ciphertext.decode('hex')
-#  # print('ciphertext to func:', ciphertext)
-#   res = encrypt(keystream, ciphertext)
-#   return res.decode('hex')
-
-
-# #generate a keystream
-# with open('secret_build_output.txt') as fp:
-#   key1 = f.readline() #key is encryption key used for encrypting, as hex string
-
-# key2 = key1.decode('hex')
-# key2 = [ord(c) for c in key]
-
-# keystream = get_keystream(key2) #keystream is now generated
-
-
-# # encrypt the plaintext, using key and RC4 algorithm
-# plaintext = 'this is the plaintext'  # plaintext is what we will encrypt, replace it with firmware?
-# ciphertext = encrypt(keystream, plaintext)
-# #print('plaintext:', plaintext)
-# #print('ciphertext:', ciphertext)
-
-
-# #decrypt the firmware (will need this in later tool I think)
-# decrypted = decrypt(keystream, ciphertext)
-# #print('decrypted:', decrypted)
-
-
-
-# '''
