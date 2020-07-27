abca73293e26def2b8dc06c17c660c26430e3901
freed2003 <41763498+freed2003@users.noreply.github.com>
2020-07-27 14:37:49 -0700

    Update fw_protect.py

--- tools/fw_protect.py
+++ tools/fw_protect.py
@@ -64,15 +64,10 @@ def protect_firmware(infile, outfile, version, message): #Big Function - encypts
     with open('secret_build_output.txt', 'rb') as sbo:
         seed = sbo.read(16)
         a = struct.unpack('H', sbo.read(2))[0]
-        print(a)
         b = struct.unpack('H', sbo.read(2))[0]
         c = struct.unpack('H', sbo.read(2))[0]
         d = struct.unpack('H', sbo.read(2))[0]
         e = struct.unpack('H', sbo.read(2))[0]
-#         aeskey = sbo.read(16)    - - this is for no stream cipher
-#         firmkey = sbo.read(16)
-#         metakey = sbo.read(16)
-        #if we were to have a seed, would happen here??
     
     # Load firmware binary from infile
     with open(infile, 'rb') as fp:
